2fbd5aebac0d11cf08e02ca668296958a6bad7b7
Kija <kmboard1@yahoo.com>
2022-03-09 20:05:56 -0500

    message

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -8,22 +8,22 @@ var uppercaseEl = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', '
 var lowercaseEl = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'q', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var numbersEl = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbolsEl = ['@', '%', '+', '\\', '/', "'", '!', '#', '$', '^', '?', ':', ',', ')', '(', '}', '{', ']', '[', '~', '-', '_', '.'];
-var lengthEl = length;
-console.log ("length");
+var length = "";
+var parsInt= "";
+
+
 
 // Prompts
 function userPrompts() {
-  var length = prompt("How many charaters? Choose number between 8 and 128");
+  length = prompt("How many charaters? Choose number between 8 and 128");
 
   if (
     isNaN(parseInt(length)) ||
     parseInt(length) < 8 ||
     parseInt(length) > 128
-  ) 
-
-  {
-   alert ("Pick a number between 8 and 128!!")
-   userPrompts ()
+  ) {
+    alert("Pick a number between 8 and 128!!")
+    userPrompts()
   }
 
   var userLower = confirm("Would you like to incluse lowercase?");
@@ -31,15 +31,16 @@ function userPrompts() {
   var userUpper = confirm("Would you like to include uppercase letters?");
   var userSymbols = confirm("Would you like to include special characteristics?");
 
-  console.log (length,userLower, userNumbers, userUpper, userSymbols)
+  console.log(length, userLower, userNumbers, userUpper, userSymbols)
 
 
   if (!userLower && !userNumbers && !userUpper && !userSymbols) {
-    console.log ("pick something")
-    alert ("Must pick something!");
-    userPrompts ()
+    console.log("pick something")
+    alert("Must pick something!");
+    userPrompts()
   }
-
+  
+  
   var promptObj = {
     length,
     userLower,
@@ -50,60 +51,52 @@ function userPrompts() {
   return promptObj;
 }
 
-function randomElement(arr){
-  var randI = Math.floor(Math.random() * arr.length)
-  var randEl = arr[randI];
-  return randEl
-}
-
 // creates function
 function generatePassword() {
   var promptAnswers = userPrompts();
   console.log("promptsAnswers");
-  var chosenChars = []
+  chosenChars = []
 
-  if (promptAnswers.userLower){
+  if (promptAnswers.userLower) {
     chosenChars += chosenChars.concat(lowercaseEl)
     console.log(chosenChars)
   }
 
-  if (promptAnswers.userUpper){
+  if (promptAnswers.userUpper) {
     chosenChars += chosenChars.concat(uppercaseEl)
     console.log(chosenChars)
   }
 
-  if (promptAnswers.userNumbers){
-      chosenChars += chosenChars.concat(numbersEl)
-      console.log(chosenChars)
-  }
-
-  if (promptAnswers.userSymbols){
-    chosenChars += chosenChars.concat(symbolsEl)
+  if (promptAnswers.userNumbers) {
+    chosenChars += chosenChars.concat(numbersEl)
     console.log(chosenChars)
   }
 
- 
-  function length ()  {
-  for (var i = 0; i < length; i++) {
-    var length = Math.floor(Math.random() * chosenChars.length);
-    // finalPass.push(randomChar)
-  
+  if (promptAnswers.userSymbols) {
+    chosenChars += chosenChars.concat(symbolsEl)
+    console.log(chosenChars)
+  }
+   
+  return chosenChars;
+}
+
+
+function getlength() { 
+  console.log ("length");
+  for (var i = 0; i < length; i++){
+  var chosenlength = parseInt * Math.floor(Math.random());
   }
 
-}
-    
-  return chosenChars;
-
-}
-
+  return chosenlength;
+ }
 
 
 function writePassword() {
   var password = generatePassword();
+  var chosenlength = getlength();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
